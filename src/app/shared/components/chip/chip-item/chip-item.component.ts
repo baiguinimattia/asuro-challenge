6fae1a13b1af0d64d04d13b19eaf3d77b9d1398e
Mattia Baiguini <mattia.baiguini@gmail.com>
2022-06-30 21:02:52 +0300

    adding basic components

---
+++ src/app/shared/components/chip/chip-item/chip-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chip-item',
+  templateUrl: './chip-item.component.html',
+  styleUrls: ['./chip-item.component.scss']
+})
+export class ChipItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChipListComponent } from './components/chip/chip-list/chip-list.component';
+import { ChipItemComponent } from './components/chip/chip-item/chip-item.component';
+import { ChipInputComponent } from './components/chip/chip-input/chip-input.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ChipListComponent,
+    ChipItemComponent,
+    ChipInputComponent
+  ],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [CommonModule],
 })
